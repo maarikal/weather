6b4ce9bedda8a6eabade1d867949340b54d6ddad
Maarika Lukk <maarika.lukk@khk.ee>
2022-01-04 13:48:45 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", cityWeather);
+
 function weatherDataFetch(city) {
     const key = "d43b9bc94518afe99cb9ba423d828c5b"
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then(function (resp) {
@@ -5,12 +7,22 @@ function weatherDataFetch(city) {
     }) // convert data to json
         .then(function (data) {
             console.log(data);
+            drawWeather(data);
         })
         .catch(function() {
             // catch any errors
         });
 }
 
-function cityWeather(event) {
+function cityWeather() {
     weatherDataFetch("Tallinn");
+}
+
+function drawWeather(data) {
+    var celsius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector("#description").innerHTML = description;
+    document.querySelector("#temp").innerHTML= celsius + "&deg;";
+    document.querySelector("#location").innerHTML = data.name;
 }
